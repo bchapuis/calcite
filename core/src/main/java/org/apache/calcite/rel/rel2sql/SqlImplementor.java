892d0004bc4566fd2f8b60dcce928e0159bfaa92
Bertil Chapuis <bchapuis@gmail.com>
2024-02-13 11:19:58 +0100

    Remove blank line

--- core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
+++ core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
@@ -1571,7 +1571,6 @@ public abstract class SqlImplementor {
       default:
         break;
       }
-
       // fall through
     default:
       throw new AssertionError(literal + ": " + typeName);
